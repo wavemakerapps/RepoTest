38854fafeb2fb6cd1ce991062f6127235f7cd05d
appTest1 wavemaker <appTest1@wavemaker.com>
2017-12-04 10:11:38 +0000

    Google maps added New files added - [googleapis,data] Files modified - [Main,profiles,pom.xml,project-services.xml]

--- src/main/webapp/pages/Main/Main.js
+++ src/main/webapp/pages/Main/Main.js
@@ -15,3 +15,10 @@ Application.$controller("MainPageController", ["$scope", function ($scope) {
     };
 
 }]);
+
+Application.$controller("googleapisTable1Controller", ["$scope",
+	function($scope) {
+		"use strict";
+		$scope.ctrlScope = $scope;
+	}
+]);
